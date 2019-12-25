fd460c6ca13774889c7bcb9423be88bbfcd81726
foolbear <greatfoolbear@gmail.com>
2019-12-25 12:36:37 +0800

    remove test target.

--- Package.swift
+++ Package.swift
@@ -20,10 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FoolToast",
-            dependencies: []),
-        .testTarget(
-            name: "FoolToastTests",
-            dependencies: ["FoolToast"],
+            dependencies: [],
             path: "FoolToast"),
     ]
 )
