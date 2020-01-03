c99578947c30e6edbffefd1b1166569dcd4cf885
foolbear <greatfoolbear@gmail.com>
2020-01-03 23:44:09 +0800

    fix dependencies.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FoolToast",
-            dependencies: [FoolUtilities],
+            dependencies: ["FoolUtilities"],
             path: "FoolToast"),
     ]
 )
