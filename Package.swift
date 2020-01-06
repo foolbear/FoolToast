ec789b314c00c10accca32635b962187a1c080c6
foolbear <greatfoolbear@gmail.com>
2020-01-06 20:10:29 +0800

    dependencies:  https://github.com/foolbear/FoolUtilities.git

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://bitbucket.org/foolbear/foolutilities.git", from: "1.2.2"),
+        .package(url: "https://github.com/foolbear/FoolUtilities.git", from: "1.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
